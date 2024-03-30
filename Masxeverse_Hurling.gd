402089c6616e635f0871935d7f2ee584b26bd075
MasxeBoi <164790389+MasxeBoi@users.noreply.github.com>
2024-03-30 17:30:39 +0200

    Add files via upload

---
+++ Masxeverse_Hurling.gd
@@ -0,0 +1,20 @@
+extends SigilEffect
+
+# This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
+func handle_event(event: String, params: Array):
+
+	# Was card summoned / moved to the space opposing this one?
+	
+	# On the board
+	if not card.in_hand:
+		if event == "card_summoned":
+			# Fuck necromancer. All my homies hate necromancer
+			if params[0].get_node("AnimationPlayer").current_animation == "DoublePerish":
+				return
+
+			if params[0] == card:
+				smoke_em(params)
+
+
+func smoke_em(params: Array):
+	fightManager.inflict_damage(1)
