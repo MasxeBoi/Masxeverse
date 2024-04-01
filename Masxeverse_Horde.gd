a1fc89076ca42b05eff79531fb9d8db9c823d291
MasxeBoi <164790389+MasxeBoi@users.noreply.github.com>
2024-04-01 19:52:00 +0300

    Update Masxeverse_Horde.gd

--- Masxeverse_Horde.gd
+++ Masxeverse_Horde.gd
@@ -6,13 +6,20 @@ func handle_event(event: String, params: Array):
 	# attached_card_summoned represents the card bearing the sigil being summoned
 	if event == "card_summoned" and params[0] == card:
 		
-		print("Dam builder triggered!")
+		print("Gang's here! There's a horde")
 		
 		var cardSlots = slotManager.playerSlots if isFriendly else slotManager.enemySlots
 		var slot = card.slot_idx()
 		
+                		var old_data = card.card_data.duplicate()
+		old_data.sigils = old_data.sigils.duplicate()
+
+		old_data.sigils.erase("Horde")
+                #stole this stuff from the fecundity sigil, hope it works
+
+                  
 		if slot > 0 and slotManager.is_slot_empty(cardSlots[slot - 1]):
-			slotManager.summon_card(CardInfo.from_name(card.name), slot - 1, isFriendly)
+			slotManager.summon_card(old_data, slot - 1, isFriendly)
 
 		if slot < 3 and slotManager.is_slot_empty(cardSlots[slot + 1]):
-			slotManager.summon_card(CardInfo.from_name(card.name), slot + 1, isFriendly)
+			slotManager.summon_card(old_data, slot + 1, isFriendly)
