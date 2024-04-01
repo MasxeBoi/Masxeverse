6c371494169c28ffef0e05693f3567ebefe493da
MasxeBoi <164790389+MasxeBoi@users.noreply.github.com>
2024-04-01 19:55:07 +0300

    Update Masxeverse_Horde.gd

--- Masxeverse_Horde.gd
+++ Masxeverse_Horde.gd
@@ -11,13 +11,12 @@ func handle_event(event: String, params: Array):
 		var cardSlots = slotManager.playerSlots if isFriendly else slotManager.enemySlots
 		var slot = card.slot_idx()
 		
-                		var old_data = card.card_data.duplicate()
+		var old_data = card.card_data.duplicate()
 		old_data.sigils = old_data.sigils.duplicate()
 
 		old_data.sigils.erase("Horde")
-                #stole this stuff from the fecundity sigil, hope it works
+		#stole this stuff from the fecundity sigil, hope it works
 
-                  
 		if slot > 0 and slotManager.is_slot_empty(cardSlots[slot - 1]):
 			slotManager.summon_card(old_data, slot - 1, isFriendly)
 
