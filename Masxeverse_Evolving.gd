b17289a17d7c4a05c03b969351b5b92fb6fa13bd
MasxeBoi <164790389+MasxeBoi@users.noreply.github.com>
2024-04-06 15:20:37 +0300

    Add files via upload

---
+++ Masxeverse_Evolving.gd
@@ -0,0 +1,13 @@
+extends SigilEffect
+
+# This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
+func handle_event(event: String, params: Array):
+
+	# attached_card_summoned represents the card bearing the sigil being summoned
+	if event == "card_summoned" and params[0] == card and isFriendly:
+
+		var old_data = CardInfo.from_name("Worker Ant")
+
+		old_data.attack += 1
+
+		fightManager.draw_card(old_data)
