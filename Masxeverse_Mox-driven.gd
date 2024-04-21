1d9e8db0285b61ab235bec01369470f839760440
MasxeBoi <164790389+MasxeBoi@users.noreply.github.com>
2024-04-21 14:01:48 +0300

    Update Masxeverse_Mox-driven.gd

--- Masxeverse_Mox-driven.gd
+++ Masxeverse_Mox-driven.gd
@@ -6,9 +6,6 @@ func handle_event(event: String, params: Array):
 	# attached_card_summoned represents the card bearing the sigil being summoned
 	if event == "card_summoned" and params[0] == card:
 
-		var atk_acc = 0
-		var hp_acc = 0
-		
 		for card in slotManager.all_friendly_cards() if isFriendly else slotManager.all_enemy_cards():
 			if "Mox" in card.card_data.name:
 				params[0].attack += 1
