d3d69518edd1aa842223c7e5854ccabc28aa54ac
MasxeBoi <164790389+MasxeBoi@users.noreply.github.com>
2024-05-13 18:37:59 +0300

    Update Masxeverse_Ally Dependant.gd

--- Masxeverse_Ally Dependant.gd
+++ Masxeverse_Ally Dependant.gd
@@ -3,10 +3,6 @@ extends SigilEffect
 # This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
 func handle_event(event: String, params: Array):
 
-	# Don't die in hand
-	if card.get_parent().name == "PlayerHand":
-		return
-
 	# attached_card_summoned represents the card bearing the sigil being summoned
 	if (event == "card_summoned" and params[0] == card) or (event == "card_perished" and params[0] != card):
 
