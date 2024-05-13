37ffbd5776c5663ec6cc5def69af665ee1a22369
MasxeBoi <164790389+MasxeBoi@users.noreply.github.com>
2024-05-13 18:35:04 +0300

    Update Masxeverse_Ally Dependant.gd

--- Masxeverse_Ally Dependant.gd
+++ Masxeverse_Ally Dependant.gd
@@ -13,14 +13,14 @@ func handle_event(event: String, params: Array):
 		# Die if no guys
 		if not "Perish" in card.get_node("AnimationPlayer").current_animation:
 		
-		print("no adjescent allies! wah wah")
+			print("no adjescent allies! wah wah")
 		
-		var cardSlots = slotManager.playerSlots if isFriendly else slotManager.enemySlots
-		var slot = card.slot_idx()
+			var cardSlots = slotManager.playerSlots if isFriendly else slotManager.enemySlots
+			var slot = card.slot_idx()
 		
-		if slot > 0 and slotManager.is_slot_empty(cardSlots[slot - 1]):
-			card.get_node("AnimationPlayer").play("Perish")
+			if slot > 0 and slotManager.is_slot_empty(cardSlots[slot - 1]):
+				card.get_node("AnimationPlayer").play("Perish")
 
-		if slot < 3 and slotManager.is_slot_empty(cardSlots[slot + 1]):
-			card.get_node("AnimationPlayer").play("Perish")
+			if slot < 3 and slotManager.is_slot_empty(cardSlots[slot + 1]):
+				card.get_node("AnimationPlayer").play("Perish")
 		
