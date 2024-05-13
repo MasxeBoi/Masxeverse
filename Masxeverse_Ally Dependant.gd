5ddaac4ee3718cd3283baf7058317abdc705194f
MasxeBoi <164790389+MasxeBoi@users.noreply.github.com>
2024-05-13 18:38:24 +0300

    Update Masxeverse_Ally Dependant.gd

--- Masxeverse_Ally Dependant.gd
+++ Masxeverse_Ally Dependant.gd
@@ -4,7 +4,7 @@ extends SigilEffect
 func handle_event(event: String, params: Array):
 
 	# attached_card_summoned represents the card bearing the sigil being summoned
-	if (event == "card_summoned" and params[0] == card) or (event == "card_perished" and params[0] != card):
+	if event == "card_summoned" and params[0] == card:
 
 		# Die if no guys
 		if not "Perish" in card.get_node("AnimationPlayer").current_animation:
